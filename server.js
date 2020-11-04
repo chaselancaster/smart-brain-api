62d19d67d6060f534c222e63f76db30aa9452d38
Chase Lancaster <chaseblancaster@gmail.com>
2020-11-04 09:37:49 -0800

    add app.post('/signin') to test

--- server.js
+++ server.js
@@ -6,6 +6,18 @@ app.get('/', (req, res) => {
     res.send('this is working')
 })
 
+app.post('/signin', (req, res) => {
+    res.send('signin')
+})
+
 app.listen(3000, () => {
     console.log('app is running on port 3000')
 })
+
+/*
+/ --> res = this is working
+/signin --> POST = success/fail
+/register --> POST = user
+/profile/:userId --> GET = user
+/image --> PUT = user (count)
+*/
