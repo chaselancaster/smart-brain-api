be4f22d132414874450214ea7bc7c1acf114f951
Chase Lancaster <chaseblancaster@gmail.com>
2020-11-04 10:59:29 -0800

    require and use cors

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { json } = require('express');
 const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
 
 const app = express();
 
 app.use(bodyParser.json())
+app.use(cors());
 
 const database = {
     users: [
