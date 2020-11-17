094c1daf81c24ea9762d9164da29d454ce24f45a
Chase Lancaster <chaseblancaster@gmail.com>
2020-11-16 16:53:58 -0800

    change postgres const to db instead

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex')
 
-const postgres = knex({
+const db = knex({
     client: 'pg',
     connection: {
       host : '127.0.0.1',
@@ -15,7 +15,9 @@ const postgres = knex({
     }
   });
 
-postgres.select('*').from('users');
+db.select('*').from('users').then(data => {
+    console.log(data)
+})
 
 const app = express();
 
