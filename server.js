f64b7f3ad01166f2e1e47879bd9acbe40155c2ea
Chase Lancaster <chaseblancaster@gmail.com>
2020-11-16 16:54:24 -0800

    delete content inside of the database variable since I will no longer need it anymore soon

--- server.js
+++ server.js
@@ -25,24 +25,6 @@ app.use(bodyParser.json())
 app.use(cors());
 
 const database = {
-    users: [
-        {
-            id: '123',
-            name: 'John',
-            email: 'john@gmail.com',
-            password: 'cookies',
-            entries: 0,
-            joined: new Date()
-        },
-        {
-            id: '124',
-            name: 'Sally',
-            email: 'sally@gmail.com',
-            password: 'bananas',
-            entries: 0,
-            joined: new Date()
-        }
-    ]
 }
 
 app.get('/', (req, res) => {
